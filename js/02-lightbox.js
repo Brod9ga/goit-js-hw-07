92c7a9e8466313299e187d56638658a06b21403f
Brod9ga <gbonik@i.ua>
2023-04-23 17:33:42 +0300

    3

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallary = document.querySelector('.gallery')
+const imageCollection = imgCollectionCreate(galleryItems)
+
+gallary.insertAdjacentHTML("beforeend", imageCollection)
+gallary.addEventListener("click", eventImgTarget)
+
+function imgCollectionCreate(img) {
+    return img
+    .map(({preview, original, description}) =>{
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+})
+.join('')}
+
+function eventImgTarget(e){
+    e.preventDefault();
+
+if (!e.target.classList.contains('gallery__image')){return}
+const gallery = new SimpleLightbox('.gallery a',{
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
+}
